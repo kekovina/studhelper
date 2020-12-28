f7da9761b87ac5effc878ea03d797ac3785b3105
GentleGame <banderes94@gmail.com>
2020-12-28 20:18:07 +0300

    Updates

---
+++ precache-manifest.ecfb651e9caa6e8c9fc2ae33baaeb58e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb5f3baab47013355911c7933e4b26f",
+    "url": "/studhelper/index.html"
+  },
+  {
+    "revision": "c4b1de5766e9405af967",
+    "url": "/studhelper/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "425e329726e030be0fea",
+    "url": "/studhelper/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "c4b1de5766e9405af967",
+    "url": "/studhelper/static/js/2.9186cf22.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/studhelper/static/js/2.9186cf22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425e329726e030be0fea",
+    "url": "/studhelper/static/js/main.9a95eda1.chunk.js"
+  },
+  {
+    "revision": "e0204d49847ad1ff6e76",
+    "url": "/studhelper/static/js/runtime-main.9c9ea4ef.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/studhelper/static/media/persik.4e1ec840.png"
+  }
+]);
