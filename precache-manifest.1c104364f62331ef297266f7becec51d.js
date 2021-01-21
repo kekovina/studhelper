599cde06f33c2fd11e60c811747b78b5ef1a4202
GentleGame <banderes94@gmail.com>
2021-01-21 04:15:26 +0300

    Updates

---
+++ precache-manifest.1c104364f62331ef297266f7becec51d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f6db278ca4180a5fb8bf24aca88ece",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b7d45deba0e63fb0274",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9a56a576fc37e047fd66",
+    "url": "./static/css/main.25089718.chunk.css"
+  },
+  {
+    "revision": "4b7d45deba0e63fb0274",
+    "url": "./static/js/2.d28f8b3b.chunk.js"
+  },
+  {
+    "revision": "063c820672e7816084a3106b062f42e9",
+    "url": "./static/js/2.d28f8b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a56a576fc37e047fd66",
+    "url": "./static/js/main.ec7cd44a.chunk.js"
+  },
+  {
+    "revision": "dce31cdc95b4de6bdc9d",
+    "url": "./static/js/runtime-main.c434619a.js"
+  },
+  {
+    "revision": "bbe0751b9022c4538aead30fde4d26b0",
+    "url": "./static/media/0_0.bbe0751b.png"
+  },
+  {
+    "revision": "f236a8b60ebf4ad22438176c6937b043",
+    "url": "./static/media/0_1.f236a8b6.png"
+  },
+  {
+    "revision": "b5846721e7db85708f68a1727d709a39",
+    "url": "./static/media/0_2.b5846721.png"
+  },
+  {
+    "revision": "ba26d0559d1b349ac416e8cbca1d290d",
+    "url": "./static/media/1_0.ba26d055.png"
+  },
+  {
+    "revision": "19cc26df7c5676879ea7af4d497728d2",
+    "url": "./static/media/1_2.19cc26df.png"
+  },
+  {
+    "revision": "5d184b6b4444518a803120a0340010c7",
+    "url": "./static/media/1_3.5d184b6b.png"
+  },
+  {
+    "revision": "1eb61ae6a83fafd9a0100751ef773f70",
+    "url": "./static/media/1_4.1eb61ae6.png"
+  },
+  {
+    "revision": "4fd69ddc52e90af4a679c7ca1a87bc65",
+    "url": "./static/media/2_0.4fd69ddc.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9935cb376e027474231e0e8e37108848.js"
+  "./precache-manifest.1c104364f62331ef297266f7becec51d.js"
 );
 
 self.addEventListener('message', (event) => {
