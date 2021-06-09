73ae1d28a91d3cd2caf6f99df9eefc9882245077
kekovina <banderes94@gmail.com>
2021-06-09 03:21:23 +0300

    sched exam 0.2

--- src/components/Alesha.js
+++ src/components/Alesha.js
@@ -16,24 +16,27 @@ export default function Alesha({ subject, status, skeleton, home, exam }){
 				Ты ничего не прогуливаешь
 			</div>)
 	} else {
-		var color
+		var color, type
 		if(!exam){
         	color = /лекц/gi.test(subject.type) ? 'lec' : /лаб/gi.test(subject.type) ? "lab" : /практ/gi.test(subject.type) ? "pract" : "empty"
 		} else {
 			if(/экз/gi.test(subject.type)){
-				subject.type = 'Экзамен'
+				type = 'Экзамен'
 				color = 'exam'
 			} else if(/ДЗ/gi.test(subject.type)){
-				subject.type = 'Дифф. зачёт'
+				type = 'Дифф. зачёт'
 				color = 'dz'
 			} else if(/зч/gi.test(subject.type)){
-				subject.type = 'Зачёт'
+				type = 'Зачёт'
 				color = 'za4'
 			} else if(/кр|курс/gi.test(subject.type)){
-				subject.type = 'Курс.работа'
+				type = 'Курс.работа'
 				color = 'kurs'
+			}else if(/конс/gi.test(subject.type)){
+				type = 'консультация'
+				color = 'kons'
 			} else {
-				subject.type = 'Не определил'
+				type = 'Не определил'
 				color = 'empty'
 			}
 		}
@@ -48,7 +51,7 @@ export default function Alesha({ subject, status, skeleton, home, exam }){
 					<div className="subj">{subject.subj}</div>
 					<div className="prep">{subject.prep}</div>
 					<div className={`types`}>
-						<div className="type">{subject.type}</div>
+						<div className="type">{exam ? type : subject.type}</div>
 						{subject.pg && <div className={`pg`}>Подгруппа {subject.pg}</div>}
 					</div>
 					
